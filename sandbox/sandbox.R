e4a00181fddb8db7e87d54c58aaa2f8bcfb016b3
alicebeittel <alice.beittel@noaa.gov>
2024-12-03 14:45:27 -0800

    new pages and content updates

---
+++ sandbox/sandbox.R
@@ -0,0 +1,11 @@
+#' Title
+#'
+#' @param x 
+#'
+#' @return Sentence that describes output of function
+#' @export when you load a package you can have immediate access. if you have an internal function that is not releveant to user - mypackage:::internal function. This is export to user 
+#'
+#' @examples
+test <- function (x){
+  print(x)
+}
